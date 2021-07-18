a1fc56ff5d55d5ea4a96d3316fc65d1acb17b922
Crowdbotics <team@crowdbotics.com>
2021-07-18 22:56:41 +0000

    Update models

--- black_holes_28941/wsgi.py
+++ black_holes_28941/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'black_holes_28941.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "black_holes_28941.settings")
 
 application = get_wsgi_application()
